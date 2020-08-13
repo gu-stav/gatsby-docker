ece8013a5bc78b17dfdf95ee2df614a495b50e46
Gustav Pursche <pursche@posteo.de>
2020-08-13 15:36:11 +0200

    Run gatsby clean instead of manually deleting public/

--- entry.sh
+++ entry.sh
@@ -23,17 +23,17 @@ fi
 # Decide what to do
 if  [ "$1" == "develop" ]
 then
-  rm -rf $GATSBY_DIR/public
+  gatsby clean
   gatsby develop --host 0.0.0.0
 
 elif  [ "$1" == "build" ]
 then
-  rm -rf $GATSBY_DIR/public
+  gatsby clean
   gatsby build
 
 elif  [ "$1" == "stage" ]
 then
-  rm -rf $GATSBY_DIR/public
+  gatsby clean
   gatsby build
   gatsby serve --port 8000
 
